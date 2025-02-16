cfdcbc0bc1a0af82160e730ea66232384ad8f33a
rocgarci <rggilkatris@gmail.com>
2025-02-16 19:44:46 +0100

    subo ft_putchar_fd

---
+++ libft/ft_putchar_fd.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rocgarci <rocgarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/16 19:07:42 by rocgarci          #+#    #+#             */
+/*   Updated: 2025/02/16 19:44:21 by rocgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/// @brief to write a character to a file descriptor
+/// @param c is the character to be written
+/// @param fd is the file descriptor
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+/*int	main(void)
+{
+	ft_putchar_fd('A', 1);
+	return (0);
+}*/
