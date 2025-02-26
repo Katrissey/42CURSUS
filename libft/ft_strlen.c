4689f3f5c62e9f3fe1c72264bb0cd8e954505392
rocgarci <rggilkatris@gmail.com>
2025-02-26 17:50:20 +0100

    ft_strlen con mi main pasando argumentos y comaentarios

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,12 +6,15 @@
 /*   By: rocgarci <rocgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 20:34:52 by rocgarci          #+#    #+#             */
-/*   Updated: 2025/02/14 23:16:42 by rocgarci         ###   ########.fr       */
+/*   Updated: 2025/02/26 17:49:43 by rocgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/// @brief the ft_strlen() function computes the length of the string s.
+/// @param s is the string whose length is to be found.
+/// @return the number of characters that precede the terminating NUL character.
 size_t	ft_strlen(const char *s)
 {
 	char	*copia;
@@ -23,13 +26,16 @@ size_t	ft_strlen(const char *s)
 	return (0);
 }
 
-/*int	main(void)
+/*int	main(int argc, char *argv[])
 {
-	size_t	lenft;
-	size_t	lenori;
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <string to mesure>", argv[0]);
+		return (1);
+	}
+	size_t	lenft = ft_strlen(argv[1]);
+	size_t	lenori = strlen(argv[1]);
 
-	lenft = ft_strlen("¡Adoro 42!");
-	lenori = strlen("¡Adoro 42!");
 	printf("The length of the string with ft is %zu.\n", lenft);
 	printf("The length of the string with strlen is %zu.\n", lenori);
 	return (0);
