755b3b7ab00d3ee95c331e4113e0150a89e70206
rocgarci <rggilkatris@gmail.com>
2025-02-26 20:33:18 +0100

    ft_isalnum con comentarios y main pasando argumentos

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -6,42 +6,38 @@
 /*   By: rocgarci <rocgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 14:33:28 by rocgarci          #+#    #+#             */
-/*   Updated: 2025/02/14 14:37:17 by rocgarci         ###   ########.fr       */
+/*   Updated: 2025/02/26 20:32:29 by rocgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/// @brief tests for any character if it is an alphanumeric one
+///equivalent to if it is an alphabetic character or a digit.
+/// @param c int value of the character to be tested
+/// @return nonzero values if the character is alphanumeric, 0 otherwise
 int	ft_isalnum(int c)
 {
 	return ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)
 		|| (c >= 48 && c <= 57));
 }
 
-/*int	main(void)
+/*int	main(int argc, char *argv[])
 {
-	unsigned char	c;
-	unsigned char	l;
-	unsigned char	s;
-
-	c = '7';
-	l = 'F';
-	s = ']';
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <the character to check if it is an alfanumeric one>", argv[0]);
+		return (1);
+	}
+	unsigned char	c = argv[1][0];
+	
 	if (ft_isalnum(c))
-		printf("The character %c is an alphanumeric character.\n", c);
+		printf("The character %c, with ft, is alphanumeric.\n", c);
 	else
-		printf("The character %c is not an alphanumeric character.\n", c);
-	if (ft_isalnum(l))
-		printf("The character %c is an alphanumeric character.\n", l);
-	else
-		printf("The character %c is not an alphanumeric character.\n", l);
-	if (ft_isalnum(s))
-		printf("The character %c is an alphanumeric character.\n", s);
-	else
-		printf("The character %c is not an alphanumeric character.\n", s);
+		printf("The character %c with ft, is NOT alphanumeric.\n", c);
 	if (isalnum(c))
-		printf("The character %c is an alphanumeric character.\n", c);
+		printf("The character %c, with isprint, is alphanumeric.\n", c);
 	else
-		printf("The character %c is not an alphanumeric character.\n", c);
+		printf("The character %c with isprint, is NOT alphanumeric.\n", c);
 	return (0);
 }*/
