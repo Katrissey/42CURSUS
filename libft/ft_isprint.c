dcd88a6e99beeaa7ae8c4461700a524f1a4adf6c
rocgarci <rggilkatris@gmail.com>
2025-02-26 19:32:36 +0100

    ft_isprint comentado y con main pasando argumentos

--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -6,41 +6,41 @@
 /*   By: rocgarci <rocgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 14:19:43 by rocgarci          #+#    #+#             */
-/*   Updated: 2025/02/14 14:24:20 by rocgarci         ###   ########.fr       */
+/*   Updated: 2025/02/26 19:32:11 by rocgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/// @brief Function that checks if the character is printable.
+/// @param c is the character to check.
+/// @return the function returns nonzero value if the character
+///is printable, 0 otherwise.
 int	ft_isprint(int c)
 {
 	return (c >= 32 && c <= 126);
 }
 
-/*int	main(void)
+/*int	main(int argc, char *argv[])
 {
-	unsigned char	c;
-	unsigned char	s;
-	unsigned char	na;
-
-	c = '7';
-	s = ']';
-	na = 128;
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <the character to check if it is printable>", argv[0]);
+		return (1);
+	}
+	unsigned char	c = argv[1][0];
+	if (argv[1][1] != '\0')
+	{
+		fprintf(stderr, "Mistaken character. You must send an individual one.\n");
+		return (1);
+	}
 	if (ft_isprint(c))
-		printf("The character %c is a printable character.\n", c);
+		printf("The character %c, with ft, is printable\n", c);
 	else
-		printf("The character %c is not a printable character.\n", c);
-	if (ft_isprint(s))
-		printf("The character %c is a printable character.\n", s);
+		printf("The character %c with ft, is NOT printable.\n", c);
+	if (isprint(c))
+		printf("The character %c, with isprint, is printable\n", c);
 	else
-		printf("The character %c is not a printable character.\n", s);
-	if (ft_isprint(na))
-		printf("The character %c is a printable character.\n", na);
-	else
-		printf("The character %c is not a printable character.\n", na);
-	if (isprint(na))
-		printf("The character %c is a printable character.\n", na);
-	else
-		printf("The character %c is not a printable character.\n", na);
+		printf("The character %c with isprint, is NOT printable.\n", c);
 	return (0);
 }*/
