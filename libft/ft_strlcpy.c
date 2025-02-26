cd94b8b954aa26afde59f8b638359d4c808bace5
rocgarci <rggilkatris@gmail.com>
2025-02-26 17:24:15 +0100

    main pasando argumentos y competo comentarios de ft_strtlcpy

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,12 +6,18 @@
 /*   By: rocgarci <rocgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 21:29:43 by rocgarci          #+#    #+#             */
-/*   Updated: 2025/02/14 23:21:36 by rocgarci         ###   ########.fr       */
+/*   Updated: 2025/02/26 17:23:35 by rocgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/// @brief Copy the string src to dst, adding a null-terminator
+///at the end of the string, and return the length of the string.
+/// @param dst is the destination string.
+/// @param src is the source string.
+/// @param size is the full size of the destination buffer.
+/// @return the length of the copied string.
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
@@ -29,18 +35,29 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	return (i);
 }
 
-/*int	main(void)
+/*int	main(int argc, char *argv[])
 {
-	char		dst[15];
-	const char	*src = "¡Adoro 42!";
-	size_t		cpyft;
-	size_t		cpyori;
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage: %s <source string> <destination total size buffer>", argv[0]);
+		return (1);
+	}
 
-	cpyft = 0;
-	cpyori = 0;
-	cpyft = ft_strlcpy(dst, src, sizeof(dst));
-	cpyori = strlcpy(dst, src, sizeof(dst));
-	printf("The string copied with ft is %s.\n", dst);
+	size_t n = strtoul(argv[2], NULL, 10);
+
+	if (n == 0 && argv[3][0] != '0')
+	{
+		fprintf(stderr, "%s, is not a number or is an invalid number.\n", argv[3]);
+		return (1);
+	}
+	
+	char		dst[n];
+	const char	*src = argv[1];
+	size_t		cpyft = ft_strlcpy(dst, src, sizeof(dst));
+	size_t		cpyori = strlcpy(dst, src, sizeof(dst));
+
+	printf("The size of de buffer is: %zu.\n", n);
+	printf("The string copied in dst with ft is %s.\n", dst);
 	printf("The length of the string with ft is %zu.\n", cpyft);
 	printf("The length of the string with strlcpy is %zu.\n", cpyori);
 	return (0);
