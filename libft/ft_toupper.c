2141d119cf64572e4ef22f4027d4832ce5e9e4ec
rocgarci <rggilkatris@gmail.com>
2025-02-26 18:53:58 +0100

    ft_toupper con comentarios y main pasando argumentos

--- libft/ft_toupper.c
+++ libft/ft_toupper.c
@@ -6,12 +6,16 @@
 /*   By: rocgarci <rocgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 20:29:28 by rocgarci          #+#    #+#             */
-/*   Updated: 2025/02/14 20:40:31 by rocgarci         ###   ########.fr       */
+/*   Updated: 2025/02/26 18:53:37 by rocgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/// @brief the ft_toupper function converts a lower-case letter
+///to the corresponding upper-case letter.
+/// @param c is the character to be converted.
+/// @return the converted character. Otherwise, returns c as it was sent.
 int	ft_toupper(int c)
 {
 	if (c >= 97 && c <= 122)
@@ -19,18 +23,20 @@ int	ft_toupper(int c)
 	return (c);
 }
 
-/*int	main(void)
+/*int	main(int argc, char *argv[])
 {
-	unsigned char	c;
-	unsigned char	l;
-	unsigned char	s;
-
-	c = 'a';
-	l = 'F';
-	s = ']';
-	printf("The character %c in uppercase is %c.\n", c, ft_toupper(c));
-	printf("The character %c keeps as %c.\n", l, ft_toupper(l));
-	printf("The character %c keeps as %c.\n", s, ft_toupper(s));
-	printf("The character %c in uppercase is %c.\n", c, toupper(c));
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <the character to upper>", argv[0]);
+		return (1);
+	}
+	unsigned char	c = argv[1][0];
+	if (c >= 97 && c <= 122)
+	{
+		printf("The character %c in uppercase with ft is: %c.\n", c, ft_toupper(c));
+		printf("The character %c in uppercase with touppwer is: %c.\n", c, toupper(c));
+	}
+	else
+		printf("The character %c keeps as %c.\n", c, ft_toupper(c));
 	return (0);
 }*/
