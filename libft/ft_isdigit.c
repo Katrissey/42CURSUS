22f80d434abaa093cce4ef7654d28c46e99cb2bf
rocgarci <rggilkatris@gmail.com>
2025-02-26 20:45:39 +0100

    ft_isdigit con comentarios y main pasando argumentos

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -6,41 +6,38 @@
 /*   By: rocgarci <rocgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 13:49:40 by rocgarci          #+#    #+#             */
-/*   Updated: 2025/02/14 14:07:53 by rocgarci         ###   ########.fr       */
+/*   Updated: 2025/02/26 20:45:16 by rocgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isdigit(int c)
+/// @brief tests for any character if it is an alphanumeric one
+///equivalent to if it is an alphabetic character or a digit.
+/// @param c int value of the character to be tested
+/// @return nonzero values if the character is alphanumeric, 0 otherwise
+int	ft_isalnum(int c)
 {
-	return (c >= 48 && c <= 57);
+	return ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)
+		|| (c >= 48 && c <= 57));
 }
 
-/*int	main(void)
+/*int	main(int argc, char *argv[])
 {
-	unsigned char	c;
-	unsigned char	l;
-	unsigned char	s;
-
-	c = '7';
-	l = 'F';
-	s = ' ';
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <the character to check if it is a digit>", argv[0]);
+		return (1);
+	}
+	unsigned char	c = argv[1][0];
+	
 	if (ft_isdigit(c))
-		printf("The character %c is a digit.\n", c);
+		printf("The character %c, with ft, is a digit.\n", c);
 	else
-		printf("The character %c is not a digit.\n", c);
-	if (ft_isdigit(l))
-		printf("The character %c is a digit.\n", l);
-	else
-		printf("The character %c is not a digit.\n", l);
-	if (ft_isdigit(s))
-		printf("The character %c is a digit.\n", s);
-	else
-		printf("The character %c is not a digit.\n", s);
+		printf("The character %c with ft, is NOT a digit.\n", c);
 	if (isdigit(c))
-		printf("The character %c is a digit.\n", c);
+		printf("The character %c, with isdigit, is a digit.\n", c);
 	else
-		printf("The character %c is not a digit.\n", c);
+		printf("The character %c with isdigit, is NOT a digit.\n", c);
 	return (0);
 }*/
