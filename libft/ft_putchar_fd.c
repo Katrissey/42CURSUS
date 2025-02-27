597be7c899bb0ca222b1f97cb67eec508ea82338
rocgarci <rggilkatris@gmail.com>
2025-02-27 17:51:58 +0100

    Renombro correcctamente mi ft_putstr_fd y modifico comentarios y cambio main para pasarle argumentos a mi ft_putchar_fd

--- libft/ft_putchar_fd.c
+++ libft/ft_putchar_fd.c
@@ -6,13 +6,13 @@
 /*   By: rocgarci <rocgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 19:07:42 by rocgarci          #+#    #+#             */
-/*   Updated: 2025/02/16 20:16:29 by rocgarci         ###   ########.fr       */
+/*   Updated: 2025/02/27 17:50:35 by rocgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/// @brief to write a character to a file descriptor
+/// @brief to write a character into a file descriptor
 /// @param c is the character to be written
 /// @param fd is the file descriptor (1 = terminal))
 void	ft_putchar_fd(char c, int fd)
@@ -20,8 +20,13 @@ void	ft_putchar_fd(char c, int fd)
 	write(fd, &c, 1);
 }
 
-/*int	main(void)
+/*int	main(int argc, char *argv[])
 {
-	ft_putchar_fd('A', 1);
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <The character to write into a file descriptor>", argv[0]);
+		return (1);
+	}
+	ft_putchar_fd(argv[1][0], 1);
 	return (0);
 }*/
