789694b26a4e2ad30fa60a6040cc3474199ff174
rocgarci <rggilkatris@gmail.com>
2025-02-27 18:21:31 +0100

    ft_putendl_fd main pasándole argumentos y modif comentarios

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -6,13 +6,13 @@
 /*   By: rocgarci <rocgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 20:09:57 by rocgarci          #+#    #+#             */
-/*   Updated: 2025/02/16 20:16:51 by rocgarci         ###   ########.fr       */
+/*   Updated: 2025/02/27 18:20:51 by rocgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/// @brief to write a string to a file descriptor followed by a newline
+/// @brief to write a string into a file descriptor followed by a newline
 /// @param s is the string to be written
 /// @param fd is the file descriptor (1 = terminal))
 void	ft_putendl_fd(char *s, int fd)
@@ -21,8 +21,13 @@ void	ft_putendl_fd(char *s, int fd)
 	ft_putchar_fd('\n', fd);
 }
 
-/*int	main(void)
+/*int	main(int argc, char *argv[])
 {
-	ft_putendl_fd("Adoro 42!", 1);
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <The string to write into a file descriptor>", argv[0]);
+		return (1);
+	}
+	ft_putendl_fd(argv[1], 1);
 	return (0);
 }*/
