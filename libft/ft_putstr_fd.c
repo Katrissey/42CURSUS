0ead501c6281030d0abe9e6749972fa7cece1703
rocgarci <rggilkatris@gmail.com>
2025-02-27 18:02:12 +0100

    ft_putstr_fd modificando comentarios y main pasando argumentos

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putsrt_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rocgarci <rocgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 19:47:36 by rocgarci          #+#    #+#             */
-/*   Updated: 2025/02/16 20:16:32 by rocgarci         ###   ########.fr       */
+/*   Updated: 2025/02/27 18:01:34 by rocgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/// @brief to write a string to a file descriptor
+/// @brief to write a string into a file descriptor
 /// @param s is the string to be written
 /// @param fd is the file descriptor (1 = terminal))
 void	ft_putstr_fd(char *s, int fd)
@@ -20,8 +20,13 @@ void	ft_putstr_fd(char *s, int fd)
 	write(fd, s, ft_strlen(s));
 }
 
-/*int	main(void)
+/*int	main(int argc, char *argv[])
 {
-	ft_putstr_fd("Adoro 42!", 1);
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <The string to write into a file descriptor>", argv[0]);
+		return (1);
+	}
+	ft_putstr_fd(argv[1], 1);
 	return (0);
 }*/
