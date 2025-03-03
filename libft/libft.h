99854825c107d8637235c910eae4598749932764
rocgarci <rggilkatris@gmail.com>
2025-03-03 18:29:51 +0100

    añado definición de la estructura de una lista vacía así como las definiciones de las funciones de las listas del bonus a mi libft.h. También añado los prototipos de las funciones de las listas del bonus a mi Makefile, así como nuevas reglas para llamar a la parte bonus además de a la parte obligatoria o sólo a la parte obligatoria de las funciones.

--- libft/libft.h
+++ libft/libft.h
@@ -6,21 +6,29 @@
 /*   By: rocgarci <rocgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 16:34:57 by rocgarci          #+#    #+#             */
-/*   Updated: 2025/02/17 13:47:33 by rocgarci         ###   ########.fr       */
+/*   Updated: 2025/03/03 18:27:34 by rocgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-# include <stdio.h>       // printf, scanf, perror
-# include <unistd.h>      // write, read, close
-# include <stdlib.h>      // malloc, free, exit
-# include <string.h>      // memset, memcpy, memmove, memchr, memcmp,
+# include <stdio.h>       // used for printf, scanf, perror
+# include <unistd.h>      // used for write, read, close
+# include <stdlib.h>      // used for malloc, free, exit, strtoul
+# include <string.h>      // used for memset, memcpy, memmove, memchr, memcmp,
 //strlen, strdup, strcpy, strncpy, strcat, strncat, strlcat, strchr,
 //strrchr, strstr, strnstr, strcmp, strncmp, atoi
-# include <ctype.h>       // isalnum, isalpha, isascii, isdigit,
+# include <ctype.h>       // used for isalnum, isalpha, isascii, isdigit,
 //isprint, tolower, toupper
-# include <bsd/string.h>  // bzero
+# include <bsd/string.h>  // used for bzero
+
+// List structure definition
+// t_list is the alias of s_list
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
 
 int		ft_atoi(const char *str);
 int		ft_isalnum(int c);
@@ -51,9 +59,18 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
-char	**ft_split(char const *s, char c);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+/*char	**ft_split(char const *s, char c);
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));*/
 #endif
