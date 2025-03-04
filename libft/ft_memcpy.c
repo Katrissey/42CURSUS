909ce85748e716259adc0556e601688a8cddfd1d
rocgarci <rggilkatris@gmail.com>
2025-03-04 13:04:40 +0100

    ft_memcpy con cpmentarios y comprobación que mi función y mi main, sin pasar argumentos aún, funcionan

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,12 +6,20 @@
 /*   By: rocgarci <rocgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 13:00:19 by rocgarci          #+#    #+#             */
-/*   Updated: 2025/02/15 19:18:25 by rocgarci         ###   ########.fr       */
+/*   Updated: 2025/03/04 13:03:57 by rocgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/// @brief copies "n" bytes from memory area "src" to memory area "dest".
+/// The memory area must not overlap
+/// @param dest the empty memory area in which you want to copy "n" bytes
+/// from the memory area "src"
+/// @param src The memory area source, from you want to copy "n" bytes
+/// into the memory area of destination "dest"
+/// @param n the number of bytes from "src" you want to copy in "dest"
+/// @return a pointer to "dest"
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned char	*ptrdest;
@@ -32,11 +40,9 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	char	src[] = "¡Adoro 42!";
 	char	dest[15];
-	char	*dest_ft;
-	char	*dest_ori;
 
-	dest_ft = ft_memcpy(dest, src, sizeof(src));
-	dest_ori = memcpy(dest, src, sizeof(src));
+	char	*dest_ft = ft_memcpy(dest, src, sizeof(src));
+	char	*dest_ori = memcpy(dest, src, sizeof(src));
 	printf("The string to copy from the source is %s.\n", src);
 	printf("The number of characters to move is %zu.\n", sizeof(src));
 	printf("The string copied with ft is %s.\n", dest_ft);
