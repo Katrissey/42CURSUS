30537eaccc765e9ec04535c591fbd3ebb8d8d8ad
rocgarci <rggilkatris@gmail.com>
2025-03-04 18:36:24 +0100

    ft_memcmp con comentarios y pruebo que funciona mi función y mi main, aún sin pasarle argumentos

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,12 +6,24 @@
 /*   By: rocgarci <rocgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 15:34:38 by rocgarci          #+#    #+#             */
-/*   Updated: 2025/02/15 19:18:03 by rocgarci         ###   ########.fr       */
+/*   Updated: 2025/03/04 18:35:42 by rocgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
+/// @brief compares the first "n" bytes of the memory areas "s1" and "s2",
+/// each of them, interpreted as unsigned char
+/// @details if some bytes from s1 and s2 match in the "n" bytes scanned,
+/// but not the all "n" bytes, the function compares de value of the first
+/// single different byte in each memory area, and substracts the ASCII value
+/// of the first, from the ASCII value of the second.
+/// @param s1 the memory area to compare its first "n" bytes to the first
+/// "n" bytes from "s2"
+/// @param s2 the other memory area to compare its first "n" bytes to the first
+/// "n" bytes from "s1"
+/// @param n number of bytes you want to compare between "s1" and "s2"
+/// @return an integer less than, equal to, or bigger than zero if the first "n"
+/// bytes of "s1" are found, respectively, to be less than, to match,
+/// or be greater than the first "n" bytes of "s2".
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned char	*ptrs1;
@@ -34,11 +46,9 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	char	str1[] = "¡Adoro 42!";
 	char	str2[] = "¡Adoro por siempre 42!";
 	size_t	n = 10;
-	int		ret_ft;
-	int		ret_ori;
+	int		ret_ft = ft_memcmp(str1, str2, n);
+	int		ret_ori = memcmp(str1, str2, n);
 
-	ret_ft = ft_memcmp(str1, str2, n);
-	ret_ori = memcmp(str1, str2, n);
 	printf("The first string to compare is %s.\n", str1);
 	printf("The second string to compare is %s.\n", str2);
 	printf("The number of characters to compare is %zu.\n", n);
