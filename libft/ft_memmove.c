c1d2660832d148b08e8ed77be4d360e1dad59997
rocgarci <rggilkatris@gmail.com>
2025-03-04 13:22:31 +0100

    ft_memmove con comentarios y pruebo mi función y mi main aún sin pasar argumentos, y funcionan bine

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,12 +6,20 @@
 /*   By: rocgarci <rocgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 13:18:06 by rocgarci          #+#    #+#             */
-/*   Updated: 2025/02/15 19:17:44 by rocgarci         ###   ########.fr       */
+/*   Updated: 2025/03/04 13:21:56 by rocgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/// @brief copies "n" bytes from the memory area "src" to memory area "dest".
+/// The memory areas may overlap, if that happends, function have to work,
+/// copying the "src" bytes into a temporary array that does not overlap "src"
+/// or "dest", and the bytes are copied then from the temporary array to "dest"
+/// @param dest the empty memory area of destination
+/// @param src the memory area you want to copy into "dest"
+/// @param n the number of bytes you want to copy into "dest" from "src"
+/// @return a pointer to "dest"
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char	*ptrdest;
@@ -40,11 +48,9 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	char	src[] = "¡Adoro 42!";
 	char	dest[15];
-	char	*dest_ft;
-	char	*dest_ori;
+	char	*dest_ft = ft_memmove(dest, src, sizeof(src));
+	char	*dest_ori = memmove(dest, src, sizeof(src));
 
-	dest_ft = ft_memmove(dest, src, sizeof(src));
-	dest_ori = memmove(dest, src, sizeof(src));
 	printf("The string to copy from the source is %s.\n", src);
 	printf("The number of characters to move is %zu.\n", sizeof(src));
 	printf("The string copied with ft is %s.\n", dest_ft);
