dff68908f430df0f5344515d7703f78c46950c46
rocgarci <rggilkatris@gmail.com>
2025-03-12 18:20:49 +0100

    añado comentarios a mi ft_strjoin y cambio mi main para que reciba argumentos por terminal

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,16 +6,17 @@
 /*   By: rocgarci <rocgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 02:56:58 by rocgarci          #+#    #+#             */
-/*   Updated: 2025/02/17 03:08:42 by rocgarci         ###   ########.fr       */
+/*   Updated: 2025/03/12 18:20:10 by rocgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/// @brief to concatenate two strings
-/// @param s1 is the first string
-/// @param s2 is the second string
-/// @return the pointer to the new string
+/// @brief to concatenate two strings, using malloc to allocate memory
+/// enough to the lenght of both string plus the terminated character '\0'
+/// @param s1 is the first string to join
+/// @param s2 is the second string to join
+/// @return the new string or NULL if the memory allocation fails
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*ptr;
@@ -35,12 +36,19 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	return (ptr);
 }
 
-/*int	main(void)
+/*int	main(int argc, char *argv[])
 {
-	char	*ptr;
-
-	ptr = ft_strjoin("¡Adoro ", "42!");
-	printf("%s\n", ptr);
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage: %s <the first string you want to join> <the second string you want to join>\n", argv[0]);
+		return (1);
+	}
+	char	*s1 = argv[1];
+	char	*s2 = argv[2];
+	char	*ptr = ft_strjoin(s1, s2);
+	printf("The first string to concatenate is: %s\n", s1);
+	printf("The second string to concatenate is: %s\n", s2);
+	printf("The new string resultant after concatenation is: %s\n", ptr);
 	free(ptr);
 	return (0);
 }*/
