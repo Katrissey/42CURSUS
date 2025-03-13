624b772b72ef40672fe7d89b689ece9711964c92
rocgarci <rggilkatris@gmail.com>
2025-03-13 22:23:00 +0100

    ft_lstadd_front con main pasando arguments y liberando memoria correctamente

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: rocgarci <rocgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 19:28:20 by rocgarci          #+#    #+#             */
-/*   Updated: 2025/03/06 16:46:13 by rocgarci         ###   ########.fr       */
+/*   Updated: 2025/03/13 22:22:19 by rocgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,42 @@
 /// at the begining of that list
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	new->next = lst;
-	*lst = new;
+	if (lst && new)
+	{
+		new->next = *lst;
+		*lst = new;
+	}
 }
+
+/*int	main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage: %s <content for head_node> <content for new_node>\n", argv[0]);
+		return (1);
+	}
+	char	*content_head_node = argv[1];
+	char	*content_new_node = argv[2];
+	t_list	*head = ft_lstnew(content_head_node);
+	t_list	*new_node = ft_lstnew(content_new_node);
+
+	if (!head || !new_node)
+	{
+		fprintf(stderr, "Failed to created new list element\n");
+		if (head)
+			free(head);
+		if (new_node)
+			free(new_node);
+		return (1);
+	}
+	printf("The memory address of the head node BEFORE calling ft_lstadd_front is: %p, and its content is: %s\n", (void *)head, (char *)head->content);
+	printf("The memory address of the NEW node BEFORE calling ft_lstadd_front is: %p, and its content is: %s\n", (void *)new_node, (char *)new_node->content);
+
+	ft_lstadd_front(&head, new_node);
+	printf("The memory address of the head node after calling ft_lstadd_front is: %p, and its content is: %s\n", (void *)head, (char *)head->content);
+	printf("The memory address of the NEW node after calling ft_lstadd_front is: %p, and its content is: %s\n", (void *)new_node, (char *)new_node->content);
+	
+	free(head->next);
+	free(new_node);
+	return (0);
+}*/
