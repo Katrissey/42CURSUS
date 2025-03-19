15006595b7316b462927aa26a2bb6403d9d880dc
rocgarci <rggilkatris@gmail.com>
2025-03-19 12:53:39 +0100

    ft_lstadd_back sin comentarios ni main aún, todavía no testeada

---
+++ libft/ft_lstadd_back.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rocgarci <rocgarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 12:38:52 by rocgarci          #+#    #+#             */
+/*   Updated: 2025/03/19 12:50:36 by rocgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*temp;
+
+	if (lst)
+	{
+		if (*lst == NULL)
+			*lst = new;
+		else
+		{
+			temp = *lst;
+			while (temp->next)
+				temp = temp->next;
+			temp->next = new;
+		}
+	}
+}
