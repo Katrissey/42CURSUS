316dfc155ca47400b67d2925e4ae23d148b40fbe
rocgarci <rggilkatris@gmail.com>
2025-03-19 15:16:19 +0100

    ft_lstsize ya con main pasando argumentos, con comentarios y funcionando perfectamente

--- libft/ft_lstsize.c
+++ libft/ft_lstsize.c
@@ -6,12 +6,15 @@
 /*   By: rocgarci <rocgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 11:51:19 by rocgarci          #+#    #+#             */
-/*   Updated: 2025/03/19 12:10:52 by rocgarci         ###   ########.fr       */
+/*   Updated: 2025/03/19 15:15:45 by rocgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/// @brief it counts the number of nodes from a list
+/// @param lst is the list from you want to know its number of nodes
+/// @return the number of nodes from lst
 int	ft_lstsize(t_list *lst)
 {
 	int	count;
@@ -29,7 +32,7 @@ int	ft_lstsize(t_list *lst)
 {
 	if (argc < 2)
 	{
-		fprintf(stderr, "Usage: %s, <at least the content of 2 nodes>", argv[0]);
+		fprintf(stderr, "Usage: %s, <at least the content of 1 node>\n", argv[0]);
 		return (1);
 	}
 	t_list	*list;
@@ -37,6 +40,11 @@ int	ft_lstsize(t_list *lst)
 	int		i;
 
 	list = ft_lstnew(argv[1]);
+	if (list == NULL)
+	{
+		fprintf(stderr, "Failed to created new list element.\n");
+		return (1);
+	}
 	i = 2;
 	while (i < argc)
 	{
@@ -44,6 +52,8 @@ int	ft_lstsize(t_list *lst)
 		ft_lstadd_back(&list, new_node);
 		i++;
 	}
+	printf("The memory address of the new list is: %p.\n", (void *)list);
 	printf("The size of the list is: %d.\n", ft_lstsize(list));
+	free_list(list);
 	return (0);
 }*/
